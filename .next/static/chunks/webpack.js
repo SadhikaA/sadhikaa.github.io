84dbb4ad3db6c72f67de39cbb300756c67833cd7
Sadhika Akula <sadhika.akula@berkeley.edu>
2024-07-04 18:19:05 -0700

    hw2 complete

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "26657f61b0a9ab65"; }
+/******/ 		__webpack_require__.h = function() { return "24b51076a2edbd22"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/graphics.js
+++ pages/graphics.js
@@ -3,6 +3,7 @@ import Layout from '../components/layout';
 import Head from 'next/head';
 import Image from 'next/image';
 
+/* HW 1 Images */
 import basic_test4_pixel from '../images/graphics/hw1/basic_test4_pixel.png';
 import test4_1 from '../images/graphics/hw1/test4_1.png';
 import test4_4 from '../images/graphics/hw1/test4_4.png';
@@ -19,6 +20,48 @@ import bilinear_1_side from '../images/graphics/hw1/bilinear_1_side.png';
 import bilinear_16_side from '../images/graphics/hw1/bilinear_16_side.png';
 import zero_linear from '../images/graphics/hw1/zero_linear.png';
 import zero_nearest from '../images/graphics/hw1/zero_nearest.png';
+
+/* HW 2 Images */
+import e0 from '../images/graphics/hw2/e0.png';
+import e1 from '../images/graphics/hw2/e1.png';
+import e2 from '../images/graphics/hw2/e2.png';
+import e3 from '../images/graphics/hw2/e3.png';
+import e4 from '../images/graphics/hw2/e4.png';
+import e5 from '../images/graphics/hw2/e5.png';
+import c1 from '../images/graphics/hw2/c1.png';
+import c2 from '../images/graphics/hw2/c2.png';
+import crumpled1 from '../images/graphics/hw2/crumpled 1.png';
+import crumpled2 from '../images/graphics/hw2/crumpled 2.png';
+import crumpled3 from '../images/graphics/hw2/crumpled 3.png';
+import crumpled4 from '../images/graphics/hw2/crumpled 4.png';
+import crumpled5 from '../images/graphics/hw2/crumpled 5.png';
+import crumpled6 from '../images/graphics/hw2/crumpled 6.png';
+import edge_flip from '../images/graphics/hw2/edge_flip.png';
+import edgeflip1 from '../images/graphics/hw2/edgeflip1.png';
+import edgeflip2 from '../images/graphics/hw2/edgeflip2.png';
+import flat from '../images/graphics/hw2/flat.png';
+import lvl0 from '../images/graphics/hw2/lvl0.png';
+import lvl1 from '../images/graphics/hw2/lvl1.png';
+import lvl2 from '../images/graphics/hw2/lvl2.png'; 
+import lvl3 from '../images/graphics/hw2/lvl3.png';
+import lvl4 from '../images/graphics/hw2/lvl4.png';
+import lvl5 from '../images/graphics/hw2/lvl5.png';
+import lvl6 from '../images/graphics/hw2/lvl6.png';
+import lvl7 from '../images/graphics/hw2/lvl7.png';
+import phong from '../images/graphics/hw2/phong.png';
+import presplit from '../images/graphics/hw2/presplit.png';
+import presplit1 from '../images/graphics/hw2/presplit1.png';
+import presplit2 from '../images/graphics/hw2/presplit2.png';
+import presplit3 from '../images/graphics/hw2/presplit3.png';
+import presplit4 from '../images/graphics/hw2/presplit4.png';
+import split from '../images/graphics/hw2/split.png';
+import splitflip from '../images/graphics/hw2/splitflip.png';
+import tea0 from '../images/graphics/hw2/tea0.png';
+import tea1 from '../images/graphics/hw2/tea1.png';
+import teapot_gray from '../images/graphics/hw2/teapot_gray.png';
+import teapot from '../images/graphics/hw2/teapot.png';
+import cow0 from '../images/graphics/hw2/cow0.png';
+import cow1 from '../images/graphics/hw2/cow1.png';
 
 export default function Graphics() {
 return (
@@ -148,12 +191,119 @@ return (
                 <caption>[L_NEAREST and P_NEAREST] and [L_NEAREST and P_LINEAR]</caption>
             </div>
             <h3 id="2">Mesh Geometry</h3>
+            <p>In this homework, the goal was to implement a geometric modeling techniques. I was able to implement the topics we had learnt in class including Bezier curves and practice half edge mesh representations throughout this homework. The most interesting part (and challenging) was understanding and correctly using the half edge mesh representation. I found that drawing images of before and after and writing out the pointers before coding to be helpful for this. I've had previous experience doing modeling in Maya so it was really cool to see the math and look at what happens to each triangle when you add subdivisions or box model.</p>
             <h4 id="2a">Bezier Curves with 1D de Casteljau Subdivision</h4>
+            <p>De Casteljau's algorithm works by using recursive linear interpolation to evaluate points on Bezier curves. Linear interpolation works by computing the control points at the previous level, based on <code>t</code>. Given an original <code>n</code> points, we evaluate <code>n - 1</code> points to get a new set of points. We recursively repeat this until we get a final point.</p>
+            <div className={styles.imageContainer}>
+                <Image src={e0} alt="robot" width={200} className={styles.mobileImage}/>
+                <Image src={e1} alt="robot" width={200} className={styles.mobileImage}/>
+                <Image src={e2} alt="robot" width={200} className={styles.mobileImage}/>
+            </div>
+            <div className={styles.imageContainer}>
+                <caption>step 1, step 2, step 3</caption>
+            </div>
+            <div className={styles.imageContainer}>
+                <Image src={e3} alt="robot" width={200} className={styles.mobileImage}/>
+                <Image src={e4} alt="robot" width={200} className={styles.mobileImage}/>
+                <Image src={e5} alt="robot" width={200} className={styles.mobileImage}/>
+            </div>
+            <div className={styles.imageContainer}>
+                <caption>step 3, step 4, step 5</caption>
+            </div>
+            <div className={styles.imageContainer}>
+                <Image src={c1} alt="robot" width={200} className={styles.mobileImage}/>
+                <Image src={c2} alt="robot" width={200} className={styles.mobileImage}/>
+            </div>
+            <div className={styles.imageContainer}>
+                <caption>modifying parameter <code>t</code></caption>
+            </div>
             <h4 id="2b">Bezier Curves with Seperable 1D de Casteljau</h4>
+            <p>First, I extend de Casteljau to Bezier surfaces by first calculating the <code>evaluateStep</code> on 3D points instead of 2D points. I then implemented the <code>evaluate1D</code> function by recursively calling <code>evaluateStep</code> for all n points and returning the final point. Finally, the <code>evaluate</code> function first evaluates <code>n</code> vectors using <code>u</code> as our parameter for <code>t</code>. Then, I evaluate these vectors in another axis using <code>v</code> as our parameter, which returns our final point. Ultimately, we extend to one more dimension to compute Bezier surfaces.</p>
+            <div className={styles.imageContainer}>
+                <Image src={teapot} alt="robot" width={250} className={styles.mobileImage}/>
+                <Image src={teapot_gray} alt="robot" width={250} className={styles.mobileImage}/>
+            </div>
+            <div className={styles.imageContainer}>
+                <caption>🫖 teapot!</caption>
+            </div>
             <h4 id="2c">Area-Weighted Vertex Normals</h4>
+            <p>In order have cleaner Phong shading, we calculate area-weighted vertex normals for each face. In order to do this, I first found all vertices for a specific face. To find the normal vector to this face, I took the cross product of two perpendicular vectors and made sure that the normal was pointing outwards. Finally, I summed all the normals across all of the faces and returned the normalized output. Since I'm not modifying any halfedges, I also made sure to use <code>HalfEdgeCIter.</code></p>
+            <div className={styles.imageContainer}>
+                <Image src={flat} alt="robot" width={250} className={styles.mobileImage}/>
+                <Image src={phong} alt="robot" width={250} className={styles.mobileImage}/>
+            </div>
+            <div className={styles.imageContainer}>
+                <caption>flat shading / phong shading using normals</caption>
+            </div>
             <h4 id="2d">Edge Flip</h4>
+            <div className={styles.imageContainer}>
+                <Image src={edge_flip} alt="robot" width={450} className={styles.mobileImage}/>
+            </div>
+            <p>In order to implement the edge flip algorithm, I first drew out this picture above and then wrote out all the pointers that changed throughout the flip and updated them accordingly in the code. Since edge flip doesn't require adding elements or deleting them, I found the implementation pretty straightforward and didn't face any bugs. I realized after completing this that a few operations and assignments could be simplified and accordinly updated that in my edge split implementation.</p>
+            <div className={styles.imageContainer}>
+                <Image src={edgeflip1} alt="robot" width={250} className={styles.mobileImage}/>
+                <Image src={edgeflip2} alt="robot" width={250} className={styles.mobileImage}/>
+            </div>
             <h4 id="2e">Edge Split</h4>
+            <p>To implement, edge split I followed similar steps as edge flip but also added in a new vertex, two additional faces, and new halfedges and edges. I found the <code>setNeighbors</code>function to be quite useful here in order to update all of the pointers of one halfedge at once. Initially, I found no issues with my edge split function but when completing upsampling (below) I realized that a few halfedges were being modified incorrectly which allowed me to fix my implementation. I also added support for labeling the new vertices and edges with <code>isNew = true</code> in the edge split function so that when I edge split in <code>unsample</code> method only the new edges will be flipped.</p>
+            <div className={styles.imageContainer}>
+                <Image src={split} alt="robot" width={250} className={styles.mobileImage}/>
+                <Image src={splitflip} alt="robot" width={250} className={styles.mobileImage}/>
+            </div>
+            <div className={styles.imageContainer}>
+                <caption>edge split / edge split and flip</caption>
+            </div>
             <h4 id="2f">Loop Subdivision for Mesh Upsampling</h4>
+            <p>I implemented loop subdivision by following the guidelines in the code. I started off by calculating and saving the new positions of the vertices to be updated in the future, using the equation <code>(1 - n * u) * v-{'>'}position + u * neighborPositionSum</code>. I then computed the vertex position of a current edge using the values of the vertices sharing an edge and the equation <code>(3.0 / 8.0) * (A-{'>'}position + B-{'>'}position) + (1.0 / 8.0) * (C-{'>'}position + D-{'>'}position)</code>. I also saved a list of <code>originalEdges</code> during this process so that I could split only the mesh edges that were from the original mesh in order to not run in an infinite loop. Here, I updated my <code>edgeSplit</code> function to mark new edges as new so that I wouldn't overflip edges. I then flipped edges based on whether it was connecting an old and new vertex and if it was a new edge. Finally, I updated the position of all the vertices.</p>
+            <p>As we can see below, the mesh looks nothing like a cube as we increase subdivisions. We achieve a more spherical shape over time but not truly spherical as it maintains part of its squareness.</p>
+            <div className={styles.imageContainer}>
+                <Image src={lvl0} alt="robot" width={150} className={styles.mobileImage}/>
+                <Image src={lvl1} alt="robot" width={150} className={styles.mobileImage}/>
+                <Image src={lvl2} alt="robot" width={150} className={styles.mobileImage}/>
+                <Image src={lvl3} alt="robot" width={150} className={styles.mobileImage}/>
+            </div>
+            <div className={styles.imageContainer}>
+                <caption>level 0 / level 1 / level 2 / level 3</caption>
+            </div>
+            <div className={styles.imageContainer}>
+                <Image src={lvl4} alt="robot" width={150} className={styles.mobileImage}/>
+                <Image src={lvl5} alt="robot" width={150} className={styles.mobileImage}/>
+                <Image src={lvl6} alt="robot" width={150} className={styles.mobileImage}/>
+                <Image src={lvl7} alt="robot" width={150} className={styles.mobileImage}/>
+            </div>
+            <div className={styles.imageContainer}>
+                <caption>level 4 / level 5 / level 6 / level 7</caption>
+            </div>
+            <p>By pre-splitting edges, we are able to reduce the sharp corners and maintain the shape of the original image. As we can see below, presplitting only one face keeps the flatness preserved while increasing subdivisions. This preprocessing helps because it creates more edges that can be subdivided which means we make even more edges when we flip, essentially adding more detail at each level.</p>
+            <div className={styles.imageContainer}>
+                <Image src={presplit} alt="robot" width={120} className={styles.mobileImage}/>
+                <Image src={presplit1} alt="robot" width={120} className={styles.mobileImage}/>
+                <Image src={presplit2} alt="robot" width={120} className={styles.mobileImage}/>
+                <Image src={presplit3} alt="robot" width={120} className={styles.mobileImage}/>
+                <Image src={presplit4} alt="robot" width={120} className={styles.mobileImage}/>
+            </div>
+            <div className={styles.imageContainer}>
+                <caption>presplit / level 1 / level 2 / level 3 / yam 🍠</caption>
+            </div>
+            <p>The usefulness of loop subdivision can be seen on the teapot and the cow which achieves a less geometric look and reduces sharp corners using these operations.</p>
+            <div className={styles.imageContainer}>
+                <Image src={tea0} alt="robot" width={150} className={styles.mobileImage}/>
+                <Image src={tea1} alt="robot" width={150} className={styles.mobileImage}/>
+                <Image src={cow0} alt="robot" width={150} className={styles.mobileImage}/>
+                <Image src={cow1} alt="robot" width={150} className={styles.mobileImage}/>
+            </div>
+            <div className={styles.imageContainer}>
+                <caption>teapot before / teapot after / cow before / cow after </caption>
+            </div>
+            <p>Coding up loop subdivision took a bit longer than the other parts due to some pointer bugs and keeping track of which halfedges were new, which led to some crumpled meshes during the debugging:</p>
+            <div className={styles.imageContainer}>
+                <Image src={crumpled1} alt="robot" width={100} className={styles.mobileImage}/>
+                <Image src={crumpled2} alt="robot" width={100} className={styles.mobileImage}/>
+                <Image src={crumpled3} alt="robot" width={100} className={styles.mobileImage}/>
+                <Image src={crumpled4} alt="robot" width={100} className={styles.mobileImage}/>
+                <Image src={crumpled5} alt="robot" width={100} className={styles.mobileImage}/>
+                <Image src={crumpled6} alt="robot" width={100} className={styles.mobileImage}/>
+            </div>
             <h3 id="3">Ray Tracing</h3>
             <h4 id="3a">Ray Generation and Scene Intersection</h4>
             <h4 id="3b">Bounding Volume Hierarchy</h4>
